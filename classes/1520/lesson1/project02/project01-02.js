1234bd29817408f04166f89227bc2610f4d22ccf
gvado0404 <glordiv9258@student.gwinnetttech.edu>
2025-01-19 20:20:31 -0500

    Add files via upload

---
+++ classes/1520/lesson1/project02/project01-02.js
@@ -0,0 +1,14 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Gabriel Lordi-Vado
+    Date:   1/17/2025
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+
+let service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate";
+let service1Speed = "0 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig";
