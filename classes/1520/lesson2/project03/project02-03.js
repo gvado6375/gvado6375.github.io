a905c3e5a2226a0d4758cd801458cbd0104f12db
gvado0404 <glordiv9258@student.gwinnetttech.edu>
2025-01-22 01:54:06 -0500

    Add files via upload

---
+++ classes/1520/lesson2/project03/project02-03.js
@@ -0,0 +1,34 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-03
+
+      Application to return the shape of a clicked object
+      Author: Gabriel Lordi-Vado
+      Date:   1/20/2025
+
+      Filename: project02-03.js
+ */
+
+document.getElementById("square").onmouseover = function() {
+	document.getElementById("feedback").innerHTML = "You\'re hovering over the square";
+}
+
+document.getElementById("square").onmouseout = function() {
+	document.getElementById("feedback").innerHTML = "";
+}
+
+document.getElementById("triangle").onmouseover = function() {
+	document.getElementById("feedback").innerHTML = "You\'re hovering over the triangle";
+}
+
+document.getElementById("triangle").onmouseout = function() {
+	document.getElementById("feedback").innerHTML = "";
+}
+
+document.getElementById("circle").onmouseover = function() {
+	document.getElementById("feedback").innerHTML = "You\'re hovering over the circle";
+}
+
+document.getElementById("circle").onmouseout = function() {
+	document.getElementById("feedback").innerHTML = "";
+}
